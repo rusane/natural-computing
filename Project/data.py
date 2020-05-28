a341fceb6723310e9f899c6538a01eb624835700
Gansen, A. (Anna) <a.gansen@student.ru.nl>
2020-05-28 15:12:15 +0200

    add correlation experiment

--- Project/data.py
+++ Project/data.py
@@ -9,7 +9,7 @@ from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import MinMaxScaler
 
 
-def preprocess(df):
+def preprocess(df, isCorr = False):
     """
     Preprocesses the data.
 
@@ -31,6 +31,22 @@ def preprocess(df):
     # select columns
     df = df[getting_started]
     
+    # based on EDA remove correlated features per class of biomarkers
+    # Cognitive tests:
+    # ADAS11 because it might be too correlated with DX_bl, MMSE because of missing values; we keep both CDRSB and RAVLT_immediate, since they are not correlated
+    
+    # MRI:
+    # Entorhinal, MidTemp, Hippocampus based on nr of missing values
+    
+    # PET:
+    # AV45 based on nr of missing values
+    
+    # CSF:
+    # TAU_UPENNBIOMK9_04_19_17 (tau level), PTAU_UPENNBIOMK9_04_19_17 (phosphorylated tau level)
+    
+    if isCorr:
+        df = df.drop(columns=["ADAS11", "Entorhinal", "MidTemp", "Hippocampus", "TAU_UPENNBIOMK9_04_19_17", "PTAU_UPENNBIOMK9_04_19_17", "MMSE"])
+        
     # filter out only baseline visits
     df = df[df["VISCODE"] == "bl"].reset_index(drop=True).drop(columns='VISCODE')
     
@@ -41,7 +57,10 @@ def preprocess(df):
     df = df[df['DX'] != 'nan'].reset_index(drop=True)
     
     # fix "ABETA_UPENNBIOMK9_04_19_17, TAU_UPENNBIOMK9_04_19_17, PTAU_UPENNBIOMK9_04_19_17" column
-    err_cols = ["ABETA_UPENNBIOMK9_04_19_17", "TAU_UPENNBIOMK9_04_19_17", "PTAU_UPENNBIOMK9_04_19_17"]
+    if isCorr:
+        err_cols = ["ABETA_UPENNBIOMK9_04_19_17"]
+    else:
+        err_cols = ["ABETA_UPENNBIOMK9_04_19_17", "TAU_UPENNBIOMK9_04_19_17", "PTAU_UPENNBIOMK9_04_19_17"]
     for c in err_cols:
         df[c] = df[c].apply(lambda x: None if x == ' ' or '<' in x or '>' in x else x)
         df[c] = df[c].astype(float)
@@ -61,7 +80,7 @@ def preprocess(df):
     
     # handle missing data (imputation by interpolation)
     impcols = [c for c in df.columns if c not in ['RID', 'DX_bl']]
-    features = df.drop(columns=['RID', 'DX_bl']).to_numpy()    
+    features = df.drop(columns=['RID', 'DX_bl']).to_numpy()  
     features = SimpleImputer(missing_values=np.nan, strategy='mean').fit(features).transform(features)
     df[impcols] = pd.DataFrame(features)
     
@@ -71,7 +90,7 @@ def preprocess(df):
     return df, label_dict
 
 
-def get_data(path):
+def get_data(path, isCorr = False):
   """
   Main function to retrieve preprocessed data.
 
@@ -84,11 +103,20 @@ def get_data(path):
     label_dict = label dictionary
   """
   df = pd.read_csv(path + "TADPOLE_D1_D2.csv")
-  df, label_dict = preprocess(df)
+  df, label_dict = preprocess(df, isCorr)
   X = df.drop(columns=['DX_bl', 'RID', 'ADAS13', 'Ventricles']).to_numpy()
   y = df['DX_bl'].to_numpy()
   return X, y, label_dict
 
+def remove_correlation():
+    """
+    Based on correlation +- 0.5 removes features and only leaves one feature per class of biomarkers
+    
+    Args:
+    
+    Return:
+    """
+    
 
 if __name__ == "__main__":
     pass
